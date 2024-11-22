4b170461ad54cd637d129bc1b8357107688eed6c
elnur <atakishiyev.elnur@gmail.com>
2024-11-22 21:14:56 +0400

    last stable version

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/ffmpeg -rtsp_transport $3 -t 10 -i $1 -an -c:v copy -b:v 2048k -hls_wrap 10 -f hls -hls_time 1 -lhls 1 -hls_segment_type fmp4 /usr/local/nginx/html/stream/$2
+/usr/bin/ffmpeg -rtsp_transport $3 -t 10 -i $1 -b:v 2048K -loglevel verbose -hls_wrap 10 -f hls -hls_time 1 -lhls 1 -hls_segment_type fmp4 /usr/local/nginx/html/stream/$2
